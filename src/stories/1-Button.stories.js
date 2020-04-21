59d80e9caa9958484b8956687c70a75493de56b8
Tom Coleman <tom@thesnail.org>
2020-04-21 15:11:45 +1000

    feature

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -12,7 +12,7 @@ export const Text = () => <Button onClick={action('clicked')}>Hello Button</Butt
 export const Emoji = () => (
   <Button onClick={action('clicked')}>
     <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
+      😀sxs
     </span>
   </Button>
 );
